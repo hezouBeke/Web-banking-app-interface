dd20db8e39aaf522301fc1c0c4aa8d01904f21bc
hezou <hezoubeke18@gmail.com>
2024-01-29 14:41:47 +0000

    new users account  informations display

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,19 +1,24 @@
+// dashboard.component.ts
+
 import { Component, OnInit } from '@angular/core';
 import { Account } from '../services/account.service';
 import { AccountService } from '../services/account.service';
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
- 
+
+  successMessage: string = '';  // Ajoutez cette ligne
+
   constructor(private accountService: AccountService) {}
-  
+
   createAccount(): void {
     const accountTypeElement = document.getElementById('accountType') as HTMLInputElement;
     const accountType = accountTypeElement?.value;
-  
+
     const newAccount: Account = {
       accountNumber: '123456789',
       accountType: accountType || 'personal',
@@ -21,24 +26,29 @@ export class DashboardComponent implements OnInit {
       balance: 0,
       owner: 'Nom du propriétaire',
     };
-  
-    // Ajouter le nouveau compte avec le service
+
     this.accountService.addAccount(newAccount);
+    this.sideBarVisible = false;
+    this.successMessage = 'Compte créé avec succès !';
+
+    setTimeout(() => {
+      this.successMessage = '';
+    }, 1000);
   }
 
-  sideBarVisible: boolean = false; 
- 
+  sideBarVisible: boolean = false;
+
   signOut() {
     console.log('Déconnexion');
   }
-  
-   toggleSideBar() {
-      this.sideBarVisible = !this.sideBarVisible;
-    }
-    accounts: Account[] = [];
+
+  toggleSideBar() {
+    this.sideBarVisible = !this.sideBarVisible;
+  }
+
+  accounts: Account[] = [];
 
   ngOnInit(): void {
     this.accounts = this.accountService.getAccounts();
   }
-
 }
